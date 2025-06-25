31d502ea26a5f2a13a3e54fbe0bad146d0ec2418
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2025-06-25 17:50:38 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -18,27 +18,27 @@ const footerHTML = footerIsAuthPage ? `
         </div>
     </footer>
 ` : `
-    <footer class="footer-main" style="background:#f8e9df;padding:60px 0 0 0;">
+    <footer class="footer-main" style="background:#f8e9df;padding:32px 0 0 0;font-family:inherit;">
       <div class="container text-center">
-        <h2 class="mb-3" style="font-weight:bold;">Subscribe To Get Offers In Your Inbox</h2>
-        <span style="font-size: 22px; color:#888;">Lorem ipsum dolor sit amet, adipiscing elit sed do eiusmod condimentum<br><br></span>
-        <div class="d-flex justify-content-center gap-4 mt-4 mb-3 flex-wrap" style="font-size:1.18rem;">
-          <a href="index.html" class="font-weight-bold nav-link" style="display:inline-block;min-width:110px;">Buy T-Shirts</a>
-          <a href="women.html" class="font-weight-bold nav-link" style="display:inline-block;min-width:80px;">Women</a>
-          <a href="men.html" class="font-weight-bold nav-link" style="display:inline-block;min-width:80px;">Men</a>
-          <a href="about.html" class="font-weight-bold nav-link" style="display:inline-block;min-width:80px;">About</a>
-          <a href="#contact" class="font-weight-bold nav-link" style="display:inline-block;min-width:80px;">Contact</a>
+        <h4 class="mb-2" style="font-weight:600;font-size:1.35rem;letter-spacing:0.5px;">Subscribe To Get Offers In Your Inbox</h4>
+        <span style="font-size: 1.05rem; color:#888;">Lorem ipsum dolor sit amet, adipiscing elit sed do eiusmod condimentum<br><br></span>
+        <div class="d-flex justify-content-center gap-3 mt-3 mb-2 flex-wrap" style="font-size:1rem;">
+          <a href="index.html" class="nav-link" style="display:inline-block;min-width:90px;font-weight:500;">Buy T-Shirts</a>
+          <a href="women.html" class="nav-link" style="display:inline-block;min-width:70px;font-weight:500;">Women</a>
+          <a href="men.html" class="nav-link" style="display:inline-block;min-width:70px;font-weight:500;">Men</a>
+          <a href="about.html" class="nav-link" style="display:inline-block;min-width:70px;font-weight:500;">About</a>
+          <a href="#contact" class="nav-link" style="display:inline-block;min-width:70px;font-weight:500;">Contact</a>
         </div>
-        <div class="d-flex justify-content-center gap-3 mb-5 flex-wrap">
-          <a class="ico-btn btn btn-lightt mx-2" type="button" href="https://www.facebook.com/NermeenKamalEldin" style="min-width:100px;min-height:60px;display:flex;align-items:center;justify-content:center;font-size:2rem;"><i class="fa-brands fa-facebook"></i></a>
-          <a class="ico-btn btn btn-lightt mx-2" type="button" href="https://github.com/NermeenKamal" style="min-width:100px;min-height:60px;display:flex;align-items:center;justify-content:center;font-size:2rem;"><i class="fa-brands fa-github"></i></a>
-          <a class="ico-btn btn btn-lightt mx-2" type="button" href="https://www.linkedin.com/in/nirmn-kamal/" style="min-width:100px;min-height:60px;display:flex;align-items:center;justify-content:center;font-size:2rem;"><i class="fa-brands fa-linkedin"></i></a>
-          <a class="ico-btn btn btn-lightt mx-2" type="button" href="https://www.behance.net/Nermeen_Kamal" style="min-width:100px;min-height:60px;display:flex;align-items:center;justify-content:center;font-size:2rem;"><i class="fa-brands fa-square-behance"></i></a>
+        <div class="d-flex justify-content-center gap-2 mb-4 flex-wrap">
+          <a class="ico-btn btn btn-lightt mx-1" type="button" href="https://www.facebook.com/NermeenKamalEldin" style="min-width:48px;min-height:38px;display:flex;align-items:center;justify-content:center;font-size:1.25rem;"><i class="fa-brands fa-facebook"></i></a>
+          <a class="ico-btn btn btn-lightt mx-1" type="button" href="https://github.com/NermeenKamal" style="min-width:48px;min-height:38px;display:flex;align-items:center;justify-content:center;font-size:1.25rem;"><i class="fa-brands fa-github"></i></a>
+          <a class="ico-btn btn btn-lightt mx-1" type="button" href="https://www.linkedin.com/in/nirmn-kamal/" style="min-width:48px;min-height:38px;display:flex;align-items:center;justify-content:center;font-size:1.25rem;"><i class="fa-brands fa-linkedin"></i></a>
+          <a class="ico-btn btn btn-lightt mx-1" type="button" href="https://www.behance.net/Nermeen_Kamal" style="min-width:48px;min-height:38px;display:flex;align-items:center;justify-content:center;font-size:1.25rem;"><i class="fa-brands fa-square-behance"></i></a>
         </div>
       </div>
-      <div style="background:#000;color:#fff;padding:32px 0 18px 0;margin-top:0;">
+      <div style="background:#000;color:#fff;padding:18px 0 10px 0;margin-top:0;">
         <div class="container text-center">
-          <span style="font-size:1.25rem;">Copyright &copy; 2024 ChanZel By NERMEEN</span>
+          <span style="font-size:1rem;">Copyright &copy; 2024 ChanZel By NERMEEN</span>
         </div>
       </div>
     </footer>
@@ -82,10 +82,10 @@ function translateFooter(lang) {
     
     if (footer) {
         // Check if elements exist before accessing them
-        const aboutTitle = footer.querySelector('h2');
+        const aboutTitle = footer.querySelector('h4');
         const aboutText = footer.querySelector('span');
-        const quickLinksTitle = footer.querySelector('h2');
-        const connectTitle = footer.querySelector('h2');
+        const quickLinksTitle = footer.querySelector('h4');
+        const connectTitle = footer.querySelector('h4');
         const copyrightText = footer.querySelector('.copy');
         
         if (aboutTitle) aboutTitle.textContent = t.about;
